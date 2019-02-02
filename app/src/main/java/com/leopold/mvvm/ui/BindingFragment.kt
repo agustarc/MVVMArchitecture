d085e38c1116770314c9a02ed8da991fb2d3a181
JoongWon <joongwon100@gmail.com>
2019-02-02 19:40:34 +0900

    add abstract binding fragment

---
+++ app/src/main/java/com/leopold/mvvm/ui/BindingFragment.kt
@@ -0,0 +1,24 @@
+package com.leopold.mvvm.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+/**
+ * @author Leopold
+ */
+abstract class BindingFragment<T> : Fragment() where T : ViewDataBinding {
+    @LayoutRes abstract fun getLayoutResId(): Int
+
+    protected lateinit var binding: T
+        private set
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return DataBindingUtil.inflate<T>(inflater, getLayoutResId(), container, false).apply { binding = this }.root
+    }
+}
